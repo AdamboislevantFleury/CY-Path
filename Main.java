7aa4adc4cd90673fac9b71f4aec0517df22facfc
Arepta <120413991+Arepta@users.noreply.github.com>
2023-05-16 19:22:42 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    new MaFenetre();
+    new MaFenetre(); // et deux si on veut
+    System.out.println(Thread.currentThread() + " en fin de main()");
+  }
+}
