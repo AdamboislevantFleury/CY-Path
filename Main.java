dd267b57abaeb6da77d4227145179750adcd12ad
Arepta <120413991+Arepta@users.noreply.github.com>
2023-05-22 18:22:36 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    new MaFenetre();
-    new MaFenetre(); // et deux si on veut
-    System.out.println(Thread.currentThread() + " en fin de main()");
-  }
-}
